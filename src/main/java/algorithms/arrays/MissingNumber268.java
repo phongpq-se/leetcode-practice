c6507bde1c510b8b7e4d34b54e5aec826bf4ad4a
Phong <phongpham.dev@gmail.com>
2024-02-20 22:01:42 +0700

    Add three sum

---
+++ src/main/java/algorithms/arrays/MissingNumber268.java
@@ -0,0 +1,16 @@
+package algorithms.arrays;
+
+/**
+ * @author phongpq
+ */
+public class MissingNumber268 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = (n * (n + 1)) / 2;
+        for (int i = 0; i < n; i++) {
+            sum -= nums[i];
+        }
+        return sum;
+    }
+
+}
